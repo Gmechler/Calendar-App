255014c74d1b81712f6f4b76cc815a461312e5e3
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 17:03:42 -0600

    javascript hard coded in place

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-$(document).ready(function){
+$(document).ready(function() {
+  // current date a nd time
+  let m = moment();
+  // create from ISO
+  console.log(m);
+  //   dynamicaly create new calander rows//
 
-    JSON.stringify({
-        postDate : moment()
-    }); 
-}
+  var newRow = $("<div>");
+  var hrCol = $("<div>").text("col1");
+  var aptCol = $("<div>").text("col2");
+  var saveCol = $("<div>").text("col3");
 
+  newRow.append(hrCol, aptCol, saveCol);
+  $("#calRow").append(newRow);
 
-// figure out how to pull out parts of the moment.js year, hr, 
+  newRow.attr("class", "bg-success");
+});
+
+// figure out how to pull out parts of the moment.js year, "hr",
