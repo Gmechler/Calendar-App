e125692390774275dadf299c58f19bc7bdb9e21f
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 19:10:32 -0600

    for loop creates 9 rows dynamically

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ $(document).ready(function() {
   let m = moment();
   $("#currentDay").text(m.format("dddd[,] MMMM Do"));
 
-  console.log(m.hours);
+  var currentHr = m.hour();
+  console.log(currentHr);
 
-  //   dynamicaly create new calander rows//
-  var newRow = $("<div>").attr("class", "row");
-  //   creates the hour,textarea and button columns//
-  var hrCol = $("<div>")
-    .attr("class", "col-2 mt-4 time-block")
-    .text("9:00 AM");
-  var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
-  var btnCol = $("<button>").attr(
-    "class",
-    "col-1 saveBtn fa fa-calendar-check-o b9"
-  );
-  //   appends the columns to the new row//
-  newRow.append(hrCol, aptCol, btnCol);
-  // appends the new row to the main container add id="mainContainer" to the top container.//
-  $("#mainContainer").append(newRow);
+  for (r = 0; r < 10; r++) {
+    //   dynamicaly create new calander rows//
+    var newRow = $("<div>").attr("class", "row");
+    //   creates the hour,textarea and button columns//
+    var hrCol = $("<div>")
+      .attr("class", "col-2 mt-4 time-block")
+      .text("9:00 AM");
+    var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
+    var btnCol = $("<button>").attr(
+      "class",
+      "col-1 saveBtn fa fa-calendar-check-o b9"
+    );
+    //   appends the columns to the new row//
+    newRow.append(hrCol, aptCol, btnCol);
+    // appends the new row to the main container add id="mainContainer" to the top container.//
+    $("#mainContainer").append(newRow);
+  }
 });
