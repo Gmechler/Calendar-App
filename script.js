bbf090591fd346944d81d46bc227dd9ffbf10e9b
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-06 15:28:15 -0600

    increementing id's added to textarea and button

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 
   function rowRender() {
     // let m = moment();
-
     var i = 0;
+
     for (r = 0; r < 10; r++) {
       //   dynamicaly create new calander rows//
       var newRow = $("<div>").attr("class", "row");
@@ -23,11 +23,12 @@ $(document).ready(function() {
       var hrCol = $("<div>")
         .attr("class", "col-2 mt-4 time-block")
         .text(workHrs[i] + ":00");
-      var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
-      var btnCol = $("<button>").attr(
-        "class",
-        "col-1 saveBtn fa fa-calendar-check-o b9"
-      );
+      var aptCol = $("<textarea>")
+        .attr("class", "col-8 first entertext")
+        .attr("id", "e" + workHrs[i]);
+      var btnCol = $("<button>")
+        .attr("class", "col-1 saveBtn fa fa-calendar-check-o")
+        .attr("id", "b" + workHrs[i]);
       //   appends the columns to the new row//
       newRow.append(hrCol, aptCol, btnCol);
       // appends the new row to the main container add id="mainContainer" to the top container.//
