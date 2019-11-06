b4b82b27cd0d23854132f90209195696bd8749e2
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 18:43:35 -0600

    current date added to header

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-  // current date a nd time
+  // creates the dayOfTheWeek, Month, Date, format
   let m = moment();
-  // create from ISO
-  console.log(m);
+  $("#currentDay").text(m.format("dddd[,] MMMM Do"));
+
   //   dynamicaly create new calander rows//
-  //   $("#mainContainer").html("<h1>What time is it?</h1>");
-
-  //   var newRow = $("<div>").attr("class", "row");
-  //   var hrCol = $("<div>")
-  //     .attr("class", "col-2 mt-4 time-block")
-  //     .text("9:00 AM");
-  //   var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
-  //   var btnCol = $("<btn>").attr(
-  //     "class",
-  //     "col-1 saveBtn fa fa-calendar-check-o b9"
-  //   );
-  //   newRow.append(hrCol, aptCol, btnCol);
-
-  //   $("#mainContainer").append(newRow);
+  var newRow = $("<div>").attr("class", "row");
+  //   creates the hour,textarea and button columns//
+  var hrCol = $("<div>")
+    .attr("class", "col-2 mt-4 time-block")
+    .text("9:00 AM");
+  var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
+  var btnCol = $("<btn>").attr(
+    "class",
+    "col-1 saveBtn fa fa-calendar-check-o b9"
+  );
+  //   appends the columns to the new row//
+  newRow.append(hrCol, aptCol, btnCol);
+  // appends the new row to the main container add id="mainContainer" to the top container.//
+  $("#mainContainer").append(newRow);
 
   //   var newRow = $("<div>");
   //   var hrCol = $("<div>").text("col1");
