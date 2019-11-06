0843b6caa773935c80e40651beed9a0af9a5d4fa
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 19:00:05 -0600

    centered save button icon

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   let m = moment();
   $("#currentDay").text(m.format("dddd[,] MMMM Do"));
 
+  console.log(m.hours);
+
   //   dynamicaly create new calander rows//
   var newRow = $("<div>").attr("class", "row");
   //   creates the hour,textarea and button columns//
@@ -10,7 +12,7 @@ $(document).ready(function() {
     .attr("class", "col-2 mt-4 time-block")
     .text("9:00 AM");
   var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
-  var btnCol = $("<btn>").attr(
+  var btnCol = $("<button>").attr(
     "class",
     "col-1 saveBtn fa fa-calendar-check-o b9"
   );
@@ -18,16 +20,4 @@ $(document).ready(function() {
   newRow.append(hrCol, aptCol, btnCol);
   // appends the new row to the main container add id="mainContainer" to the top container.//
   $("#mainContainer").append(newRow);
-
-  //   var newRow = $("<div>");
-  //   var hrCol = $("<div>").text("col1");
-  //   var aptCol = $("<div>").text("col2");
-  //   var saveCol = $("<div>").text("col3");
-
-  //   newRow.append(hrCol, aptCol, saveCol);
-  //   $("#calRow").append(newRow);
-
-  //   newRow.attr("class", "bg-success");
 });
-
-// figure out how to pull out parts of the moment.js year, "hr",
