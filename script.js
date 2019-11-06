6a1b03447e7b021daec2cc1ba7c245149118b0a0
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 18:16:56 -0600

    dynamic creation of one row works

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ $(document).ready(function() {
   // create from ISO
   console.log(m);
   //   dynamicaly create new calander rows//
+  //   $("#mainContainer").html("<h1>What time is it?</h1>");
 
-  var newRow = $("<div>");
-  var hrCol = $("<div>").text("col1");
-  var aptCol = $("<div>").text("col2");
-  var saveCol = $("<div>").text("col3");
+  //   var newRow = $("<div>").attr("class", "row");
+  //   var hrCol = $("<div>")
+  //     .attr("class", "col-2 mt-4 time-block")
+  //     .text("9:00 AM");
+  //   var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
+  //   var btnCol = $("<btn>").attr(
+  //     "class",
+  //     "col-1 saveBtn fa fa-calendar-check-o b9"
+  //   );
+  //   newRow.append(hrCol, aptCol, btnCol);
 
-  newRow.append(hrCol, aptCol, saveCol);
-  $("#calRow").append(newRow);
+  //   $("#mainContainer").append(newRow);
 
-  newRow.attr("class", "bg-success");
+  //   var newRow = $("<div>");
+  //   var hrCol = $("<div>").text("col1");
+  //   var aptCol = $("<div>").text("col2");
+  //   var saveCol = $("<div>").text("col3");
+
+  //   newRow.append(hrCol, aptCol, saveCol);
+  //   $("#calRow").append(newRow);
+
+  //   newRow.attr("class", "bg-success");
 });
 
 // figure out how to pull out parts of the moment.js year, "hr",
