412f459e8ad224bae432f3a22ea0e8388cf18abe
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 22:29:35 -0600

    for loop row generator works perfectly

--- script.js
+++ script.js
@@ -8,22 +8,32 @@ $(document).ready(function() {
 
   var currentHr = m.hour();
   console.log(currentHr);
+  console.log(m.toString);
 
-  for (r = 0; r < 9; r++) {
-    //   dynamicaly create new calander rows//
-    var newRow = $("<div>").attr("class", "row");
-    //   creates the hour,textarea and button columns//
-    var hrCol = $("<div>")
-      .attr("class", "col-2 mt-4 time-block")
-      .text(workHrs[0] + ":00");
-    var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
-    var btnCol = $("<button>").attr(
-      "class",
-      "col-1 saveBtn fa fa-calendar-check-o b9"
-    );
-    //   appends the columns to the new row//
-    newRow.append(hrCol, aptCol, btnCol);
-    // appends the new row to the main container add id="mainContainer" to the top container.//
-    $("#mainContainer").append(newRow);
+  var h = 0;
+
+  function rowRender() {
+    // let m = moment();
+
+    var i = 0;
+    for (r = 0; r < 10; r++) {
+      //   dynamicaly create new calander rows//
+      var newRow = $("<div>").attr("class", "row");
+      //   creates the hour,textarea and button columns//
+      var hrCol = $("<div>")
+        .attr("class", "col-2 mt-4 time-block")
+        .text(workHrs[i] + ":00");
+      var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
+      var btnCol = $("<button>").attr(
+        "class",
+        "col-1 saveBtn fa fa-calendar-check-o b9"
+      );
+      //   appends the columns to the new row//
+      newRow.append(hrCol, aptCol, btnCol);
+      // appends the new row to the main container add id="mainContainer" to the top container.//
+      $("#mainContainer").append(newRow);
+      i++;
+    }
   }
+  rowRender();
 });
