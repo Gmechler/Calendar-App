36b1afd65392831539a41205b68b4ee87f402b8a
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-05 21:19:47 -0600

    using an array to gen time not incremented yet

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  //   creates an array of hours from 8am to 5pm
+  var workHrs = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
+
   // creates the dayOfTheWeek, Month, Date, format
   let m = moment();
   $("#currentDay").text(m.format("dddd[,] MMMM Do"));
@@ -6,13 +9,13 @@ $(document).ready(function() {
   var currentHr = m.hour();
   console.log(currentHr);
 
-  for (r = 0; r < 10; r++) {
+  for (r = 0; r < 9; r++) {
     //   dynamicaly create new calander rows//
     var newRow = $("<div>").attr("class", "row");
     //   creates the hour,textarea and button columns//
     var hrCol = $("<div>")
       .attr("class", "col-2 mt-4 time-block")
-      .text("9:00 AM");
+      .text(workHrs[0] + ":00");
     var aptCol = $("<textarea>").attr("class", "col-8 first entertext e9");
     var btnCol = $("<button>").attr(
       "class",
